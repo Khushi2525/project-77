cb9ec2be02ef4b0869b6bdb6dfe7ef28f89380fa
Khushi2525 <85677072+Khushi2525@users.noreply.github.com>
2021-09-20 17:54:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name="Khushi";
+console.log(first_name);
+
+var last_name="Veeraaghattam";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a= 25;
+console.log(a);
+
+var b= 25;
+console.log(b)
+
+var add= a + b ;
+console.log(add)
